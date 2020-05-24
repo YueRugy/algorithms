118ad8a259e065b2184fc11b7ed61a9badeda961
yue <yueweichao0927@gmail.com>
2020-05-24 16:11:57 +0800

    递归斐波那契数列

---
+++ recursive/fib.go
@@ -0,0 +1,60 @@
+package recursive
+
+func fib(n int) int {
+	if n <= 2 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func fib1(n int) int {
+	if n <= 2 {
+		return 1
+	}
+	array := make([]int, n+1)
+	array[1], array[2] = 1, 1
+	return fibArray(n, &array)
+}
+
+func fib2(n int) int {
+	if n <= 2 {
+		return 1
+	}
+	array := make([]int, n+1)
+	array[1], array[2] = 1, 1
+	for index := 3; index <= n; index++ {
+		array[index] = array[index-1] + array[index-2]
+	}
+	return array[n]
+}
+
+func fib3(n int) int {
+	if n <= 2 {
+		return 1
+	}
+	rollArray := make([]int, 2, 2)
+
+	for i := 0; i < 2; i++ {
+		rollArray[i] = 1
+	}
+
+	for index := 3; index <= n; index++ { //index &(len(rollArray-1）len(rollArray)是2的n次幂时才能使用
+		rollArray[index&1]=rollArray[(index-1)&1]+rollArray[(index-2)&1]
+	}
+	return rollArray[n&1]
+}
+
+func fibArray(n int, array *[]int) int {
+	if (*array)[n] == 0 && n > 2 {
+		(*array)[n] = fibArray(n-1, array) + fibArray(n-2, array)
+	}
+	return (*array)[n]
+}
+
+func oneNumber(array *[]int) int {
+	res:=0
+	for _, number := range *array {
+		res^=number
+	}
+	return res
+}
---
+++ recursive/fib_test.go
@@ -0,0 +1,41 @@
+package recursive
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+const number = 30
+
+func TestFib(t *testing.T) {
+	fmt.Println(fib(6))
+}
+
+func TestFib1(t *testing.T) {
+	fmt.Println(fib1(6))
+}
+
+func TestFib2(t *testing.T) {
+	fmt.Println(fib2(6))
+}
+
+func TestFibTime(t *testing.T) {
+
+	timePrint(fib, number)
+	timePrint(fib1, number)
+	timePrint(fib2, number)
+	timePrint(fib3,number)
+}
+
+func TestOneNumber(t *testing.T)  {
+	arr:=&[]int{1,2,1,3,2,3,4}
+	fmt.Println(oneNumber(arr))
+}
+
+func timePrint(f func(int) int, n int) {
+	begin := time.Now()
+	fmt.Println(f(n))
+	println(time.Now().Sub(begin).String())
+}
+
